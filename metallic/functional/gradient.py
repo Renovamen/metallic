fix: use accumulated inner loop gradients instead of final gradients (#1)

metallic/functional/gradient.py
@@ -1,7 +1,16 @@
+import torch
 from torch import nn
+from typing import Optional, Sequence, List
 
-def apply_grads(model: nn.Module, grads: list) -> None:
-    """Map a list of gradients to a model."""
+def apply_grads(model: nn.Module, grads: Sequence[torch.Tensor]) -> None:
+    """
+    Map a list of gradients to a model.
+
+    Parameters
+    ----------
+    grads : Sequence[torch.Tensor]
+        List of gradient for each model parameter
+    """
     if not len(grads) == len(list(model.parameters())):
         msg = 'WARNING: Parameters and gradients have different length. ('
         msg += str(len(list(model.parameters()))) + ' vs ' + str(len(grads)) + ')'
@@ -13,3 +22,20 @@ def apply_grads(model: nn.Module, grads: list) -> None:
                 param.grad = grad.clone()
             else:
                 param.grad += grad.clone()
+
+def accum_grads(grads: Sequence[Sequence[torch.Tensor]]) -> List[torch.Tensor]:
+    """
+    Compute accumulated gradients
+
+    Parameters
+    ----------
+    grads : Sequence[Sequence[torch.Tensor]]
+        List of gradients on all previous tasks
+
+    Returns
+    -------
+    sum_grads : List[torch.Tensor]
+       Accumulated gradients
+    """
+    sum_grads = [torch.stack(g).sum(dim=0) for g in zip(*grads)]
+    return sum_grads

metallic/metalearners/gbml/maml.py
@@ -5,7 +5,7 @@ from torch import nn, optim
 
 from .base import GBML
 from ...utils import get_accuracy
-from ...functional import apply_grads
+from ...functional import apply_grads, accum_grads
 
 class MAML(GBML):
     """
@@ -123,15 +123,15 @@ class MAML(GBML):
 
                 # compute gradients when in the meta-training stage
                 if meta_train == True:
-                    # query_loss.backward()
+                    # (query_loss / n_tasks).backward()
                     outer_grad = torch.autograd.grad(query_loss / n_tasks, fmodel.parameters(time=0))
                     grad_list.append(outer_grad)
 
         # When in the meta-training stage, update the model's meta-parameters to
         # optimize the query losses across all of the tasks sampled in this batch.
         if meta_train == True:
-            # apply gradients to the original model parameters
-            apply_grads(self.model, grad_list[-1])
+            # apply accumulated gradients to the original model parameters
+            apply_grads(self.model, accum_grads(grad_list))
             # outer loop update
             self.out_optim.step()
 